e534f0060c9bbea6b73bddcb46c4363687197337
Unknown <jannis-kambach@gmx.de>
2019-01-24 16:49:06 +0100

    recommender with user input + api upodate

--- docker/python/api.py
+++ docker/python/api.py
@@ -12,13 +12,22 @@ api = Api(app)
 
 class Content(Resource):
     def get(self):
-		start = request.args.get('start')
+		recipe = request.args.get('recipe')
 		amount = request.args.get('amount')
-		output = subprocess.check_output([sys.executable, "content.py", start, amount])
+		output = subprocess.check_output([sys.executable, "content.py", recipe, amount])
         return {'recipes': output}
+		
+class User(Resource):
+    def get(self):
+		recipe = request.args.get('recipe')
+		rating = request.args.get('rating')
+		user = request.args.get('user')
+		output = subprocess.check_output([sys.executable, "user.py", recipe, rating, user])
+        return {'recipe': output}
 
 # Create routes
 api.add_resource(Content, '/content')
+api.add_resource(User, '/user')
 
 # Run the application
 if __name__ == '__main__':
